c8e2911bbab88b2c632a7d470939a130b5050d81
tinkermanz <m.b.istiak@outlook.com>
2024-06-23 05:24:06 +0600

    Star component done and many more

--- usePopcorn/src/App.jsx
+++ usePopcorn/src/App.jsx
@@ -61,6 +61,15 @@ export default function App() {
 				<NumResults movies={movies} />
 			</NavBar>
 			<Main>
+				{/* <Box element={<MovieList movies={movies} />} />
+				<Box
+					element={
+						<>
+							<WatchedSummary watched={watched} />
+							<WatchedMoviesList watched={watched} />
+						</>
+					}
+				/> */}
 				<Box>
 					<MovieList movies={movies} />
 				</Box>
--- usePopcorn/src/main.jsx
+++ usePopcorn/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from "./StarRating";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		{/* <App /> */}
+		{/* <StarRating maxRating={10} /> */}
+		<StarRating maxRating={5} />
+		<StarRating size={24} />
+	</React.StrictMode>
+);

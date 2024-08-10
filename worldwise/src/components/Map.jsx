d3ff140a3def57ed8890247238839dd0436353e6
tinkermanz <m.b.istiak@outlook.com>
2024-08-10 09:56:12 +0600

    worldwise app halfly done

---
+++ worldwise/src/components/Map.jsx
@@ -0,0 +1,20 @@
+import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function Map() {
+	const navigate = useNavigate();
+
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
+
+	return (
+		<div className={styles.mapContainer} onClick={() => navigate("form")}>
+			<h1>Map</h1>
+			<h2>
+				Position : {lat}, {lng}
+			</h2>
+		</div>
+	);
+}

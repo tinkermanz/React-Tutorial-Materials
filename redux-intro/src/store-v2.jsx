7017723ba1869ce4123da7ea3f0ec649ea5484db
tinkermanz <m.b.istiak@outlook.com>
2024-08-31 20:13:12 +0600

    fast-react-pizza folder structure done

--- redux-intro/src/store-v2.jsx
+++ redux-intro/src/store-v2.jsx
@@ -4,6 +4,8 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
+
+
 const rootReducer = combineReducers({
 	account: accountReducer,
 	customer: customerReducer,

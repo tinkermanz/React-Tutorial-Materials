e71bb18638884d7ce8df9aa777c6e7e66c951e60
tinkermanz <m.b.istiak@outlook.com>
2024-09-02 04:48:44 +0600

    fetching order done

--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -1,45 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiResturent";
 import {
 	calcMinutesLeft,
 	formatCurrency,
 	formatDate,
 } from "../../utils/helpers";
 
-const order = {
-	id: "ABCDEF",
-	customer: "Jonas",
-	phone: "123456789",
-	address: "Arroios, Lisbon , Portugal",
-	priority: true,
-	estimatedDelivery: "2027-04-25T10:00:00",
-	cart: [
-		{
-			pizzaId: 7,
-			name: "Napoli",
-			quantity: 3,
-			unitPrice: 16,
-			totalPrice: 48,
-		},
-		{
-			pizzaId: 5,
-			name: "Diavola",
-			quantity: 2,
-			unitPrice: 16,
-			totalPrice: 32,
-		},
-		{
-			pizzaId: 3,
-			name: "Romana",
-			quantity: 1,
-			unitPrice: 15,
-			totalPrice: 15,
-		},
-	],
-	position: "-9.000,38.000",
-	orderPrice: 95,
-	priorityPrice: 19,
-};
-
 export default function Order() {
+	const order = useLoaderData();
+
 	const {
 		id,
 		status,
@@ -78,4 +47,9 @@ export default function Order() {
 			</div>
 		</div>
 	);
+}
+
+export async function loader({ params }) {
+	const order = await getOrder(params.orderId);
+	return order;
 }

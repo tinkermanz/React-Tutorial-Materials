286a4c99ede5b37f65df167b9c0108b08a25acbc
tinkermanz <m.b.istiak@outlook.com>
2024-09-03 08:16:31 +0600

    react action implemented for forms

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,3 +1,8 @@
+import { Form } from "react-router-dom";
+import { createOrder } from "../../services/apiResturent";
+import { redirect } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
+
 const isValidPhone = (str) =>
 	/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
 		str
@@ -28,13 +33,17 @@ const fakeCart = [
 ];
 
 export default function CreateOrder() {
+	const navigation = useNavigation();
+
+	const isSubmitting = navigation.state === "submitting";
+
 	// const [withPriority, setWithPriority] = useState(false);
 	const cart = fakeCart;
 	return (
 		<div>
 			<h2>Ready to order? Let's go!</h2>
 
-			<form>
+			<Form method="POST">
 				<div>
 					<label>First Name</label>
 					<input type="text" name="customer" required />
@@ -64,9 +73,29 @@ export default function CreateOrder() {
 				</div>
 
 				<div>
-					<button>Order now</button>
+					<input type="hidden" name="cart" value={JSON.stringify(cart)} />
+					<button disabled={isSubmitting}>
+						{isSubmitting ? "Placing order..." : "Order now"}
+					</button>
 				</div>
-			</form>
+			</Form>
 		</div>
 	);
+}
+
+export async function action({ request }) {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+
+	const order = {
+		...data,
+		cart: JSON.parse(data.cart),
+		priority: data.priority === "on",
+	};
+
+	console.log(order);
+
+	const newOrder = await createOrder(order);
+
+	return redirect(`/order/${newOrder.id}`);
 }

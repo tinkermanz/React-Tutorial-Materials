f6207b26a91fc8bcd75e49d61379b1da85c4a249
tinkermanz <m.b.istiak@outlook.com>
2024-10-26 09:16:03 +0600

    Styled component 'as' and 'css' function used

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -2,13 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-// Tagged Template Literal
-const H1 = styled.h1`
-	font-size: 30px;
-	font-weight: 600;
-	background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
 	background-color: orangered;
@@ -19,8 +13,9 @@ export default function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<H1>The Wild Oasis</H1>
+				<Heading type="h1">The Wild Oasis</Heading>
 				<Button onClick={() => alert("Check in")}>Check in</Button>
+				<Heading as="h2">Form</Heading>
 				<Input type="number" placeholder="Number of guests" />
 			</StyledApp>
 		</>

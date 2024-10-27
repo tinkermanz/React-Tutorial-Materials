4b12c1b7f768c02c80800e4c99c9e468f16860aa
tinkermanz <m.b.istiak@outlook.com>
2024-10-27 10:54:30 +0600

    Sidebar and  Main Navigation implemented

--- the-wild-oasis/src/ui/Siderbar.jsx
+++ the-wild-oasis/src/ui/Siderbar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
 
 const StyledSiderbar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -6,8 +8,16 @@ const StyledSiderbar = styled.aside`
 	border-right: 1px solid var(--color-grey-100);
 
 	grid-row: 1 / -1;
+	display: flex;
+	flex-direction: column;
+	gap: 3.2rem;
 `;
 
 export default function Siderbar() {
-	return <StyledSiderbar>Siderbar</StyledSiderbar>;
+	return (
+		<StyledSiderbar>
+			<Logo />
+			<MainNav />
+		</StyledSiderbar>
+	);
 }

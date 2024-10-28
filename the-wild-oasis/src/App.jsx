68a0e988bfcd6bd963b4d3136a9369862eeac2ae
tinkermanz <m.b.istiak@outlook.com>
2024-10-28 09:13:56 +0600

    React query implemented data fetching query added

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -10,10 +10,21 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import { Navigate } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+		},
+	},
+});
 
 export default function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -31,6 +42,6 @@ export default function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 }
